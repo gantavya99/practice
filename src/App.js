0ce947a4fa086b25b38b8d927bf5e139f83a3ce3
Gantavya Saraswat <gantavya99@gmail.com>
2023-02-19 21:04:51 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 function App() {
+  const [json,setJson]=useState([]);
+  const [counter,setCounter]=useState(0);
+ 
+ 
+  const fetchData=()=>{
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const data = axios.get(url).then((response)=>{
+      setJson(response.data);
+      console.log(response.data)
+    }) 
+  }
+  useEffect(()=>{
+    fetchData();
+    
+  },[]);
+  setTimeout(()=>{
+    if(counter==5)
+    setCounter(0);
+    else
+    setCounter(()=>counter+1)
+  },1000)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter : {counter}</h1>
+      
+      {json.map(user=>(
+        <h2>
+          {user.name}
+        </h2>
+      ))}
+      
     </div>
   );
 }
 
 export default App;
+
+
+//fetch the data and after 10 seconds re render it . 
